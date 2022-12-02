500791a9cdcf6b8a15c407aff7aa9fe255048fa7
JoseManuel <josemanuelgarciatrave@gmail.com>
2022-12-02 11:46:30 +0100

    commit

---
+++ [Garage]/hypnonema-map/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'Example map for hypnonema'
+
+this_is_a_map 'yes'
+data_file 'DLC_ITYP_REQUEST' 'stream/v_hypnonema.ytyp'
+client_script "@BadgerAnticheat/acloader.lua"
---
+++ [packhousejohnson]/loaf_keysystem/config.lua
@@ -0,0 +1,21 @@
+Config = {
+    EnableESXIdentity = true, -- use firstname lastname format instead of steam / fivem names when giving keys
+    Command = "llaves", -- command to open the menu
+}
+
+Strings = {
+    ["main_menu"] = "Llaves",
+    ["no_keys"] = "Tu no tienes llaves.",
+    ["none_nearby"] = "No hay jugadores cerca.",
+    ["transfer_nearby"] = "Dar llave a un jugador cercano",
+    ["use"] = "Usar llave",
+    ["remove"] = "Eliminar llave",
+    ["transfer"] = "Dar llave",
+    ["no_longer_nearby"] = "El jugador ya no está cerca.",
+    ["gave_key"] = "Dio la llave a %s.",
+    ["couldnt_give"] = "No se pudo transferir la llave.",
+    ["confirm_remove"] = "Eliminar llave?",
+    ["yes"] = "Si",
+    ["no"] = "No",
+    ["recieved_key"] = "Has recibido una llave.",
+}
